4f14b6d52efd828ceb9bc90b6b638ed915a367b1
James Waterhouse <james.waterhouse@yahoo.co.uk>
2020-06-02 15:06:35 +0100

    add mikes code

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+
+$arr1 = [1,5,34,54,78,562,655,864,6543,6632,7777,7778,46266,24562];
+ 
+ $before = microtime(true);
+
+ // run your function
+
+ $after = microtime(true);
+ echo 'Time taken:' . ($after-$before);
